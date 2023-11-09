d946d6fb3e3c55cded57d490f5c242dc056a1162
SalihS2492 <144353145+SalihS2492@users.noreply.github.com>
2023-11-09 15:14:07 +0000

    scriptjs added

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("askButton").addEventListener("click", function(){
+    let answer = '';
+    let randomIndex = Math.floor(Math.random())*5;
+
+    if(randomIndex === 0){
+        answer = "It is certain.";
+    } else if (randomIndex === 1){
+        answer = "Ask again later.";
+    } else if (randomIndex === 2){
+        answer = "Without a doubt.";
+    } else if 
+});
