4f70f0975dc2aa5eca4900e6a2c728f41692aac2
SalihS2492 <144353145+SalihS2492@users.noreply.github.com>
2023-11-14 15:12:16 +0000

    Finishing product

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-document.getElementById("askButton").addEventListener("click", function(){
+//Create event Listener
+document.getElementById('askButton').addEventListener('click', function() {
     let answer = '';
-    let randomIndex = Math.floor(Math.random())*5;
+    // Generate a random number between 0 and 4 (inclusive), rounded
+    let randomIndex = Math.floor(Math.random() * 5);
 
-    if(randomIndex === 0){
+    //Use if-else-if to provide an answer based on the randomIndex
+    if (randomIndex === 0) {
         answer = "It is certain.";
-    } else if (randomIndex === 1){
+    } else if (randomIndex === 1) {
         answer = "Ask again later.";
-    } else if (randomIndex === 2){
-        answer = "Without a doubt.";
-    } else if 
+    } else if (randomIndex === 2) {
+        answer = "Yes, definitely.";
+    } else if (randomIndex === 3) {
+        answer = "My reply is no";
+    } else if (randomIndex === 4) {
+        answer = "I cannot predict now.";
+    } else {
+        answer = "Very doubtful.";
+    }
+
+//Check if  a question is asked, trim method checks for a blank input
+let question = document.getElementById('question').value;
+if (question.trim() === ''){
+    alert("Please enter a question.");
+} else{
+    document.getElementById('answer').textContent = answer;
+}
 });
